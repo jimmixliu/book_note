a4b730d88708a515dfab62ee375f962486867807
jimmixliu <lpowencom@gmail.com>
2015-10-23 13:19:08 +0800

    use namespace example

---
+++ cplusplus_tutorial/ 5_2_AC_2.cpp
@@ -0,0 +1,20 @@
+//use namespace example
+
+#include <iostream>
+
+using namespace std;
+
+namespace first{
+	int var = 5;	
+}
+
+namespace second{
+	int var = 2;
+}
+
+int main()
+{
+	using namespace first;
+	cout<<var<<endl;
+	return 0;
+}
