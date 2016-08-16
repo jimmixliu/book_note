2cf72cd81131a9086109b42fed333c9829ce98f8
jimmixliu <lpowencom@gmail.com>
2016-08-16 20:13:48 +0800

    add the second

--- ldd_cn_4_0/008/second.c
+++ ldd_cn_4_0/008/second.c
@@ -2,19 +2,31 @@
 #include <linux/init.h>
 #include <linux/cdev.h>
 #include <linux/timer.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/mm.h>
 
-#define SECOND_MAJOR 248
+#define SECOND_MAJOR 231
 
 static int second_major = SECOND_MAJOR;
 module_param(second_major, int , S_IRUGO);
 
 struct second_dev{
-	struct cdev dev;
+	struct cdev cdev;
 	atomic_t counter;
-	strcut timer_list s_timer;
+	struct timer_list s_timer;
 };
 
 static struct second_dev *second_devp;
+
+static void second_timer_handler(unsigned long arg)
+{
+	mod_timer(&second_devp->s_timer, jiffies + HZ);
+	atomic_inc(&second_devp->counter);
+
+	printk(KERN_INFO "current jiffies is %ld\n", jiffies);
+}
 
 static int second_open(struct inode *inode, struct file *filp)
 {
@@ -38,7 +50,13 @@ static int second_release(struct inode *inode, struct file *filp)
 
 static ssize_t second_read(struct file *filp, char __user *buf, size_t count, loff_t *ppos)
 {
+	int counter;
 
+	counter = atomic_read(&second_devp->counter);
+	if(put_user(counter, (int *)buf))
+		return -EFAULT;
+	else
+		return sizeof(unsigned int);
 }
 
 
@@ -46,27 +64,27 @@ static const struct file_operations second_fops = {
 	.owner = THIS_MODULE,
 	.open = second_open,
 	.release = second_release,
-	.read = second_read.
+	.read = second_read,
 };
 
 static void second_setup_cdev(struct second_dev *dev, int index)
 {
 	int err, devno = MKDEV(second_major, index);
 
-	cdev_init(&dev->cdev, &second_fops):
+	cdev_init(&dev->cdev, &second_fops);
 	dev->cdev.owner = THIS_MODULE;
 	err = cdev_add(&dev->cdev, devno, 1);
 	if(err)
 		printk(KERN_ERR "Failed to add second device\n");
 }
 
-static int __int second_int(void)
+static int __init second_init(void)
 {
 	int ret;
 	dev_t devno = MKDEV(second_major, 0);
 
 	if(second_major)
-		ret = register_chrdev_region(devno, 1 "second");
+		ret = register_chrdev_region(devno, 1, "second");
 	else{
 		ret = alloc_chrdev_region(&devno, 0, 1, "second");
 		second_major = MAJOR(devno);
@@ -96,7 +114,14 @@ module_init(second_init);
 
 static void __exit second_exit(void)
 {
-
+	cdev_del(&second_devp->cdev);
+	kfree(second_devp);
+	unregister_chrdev_region(MKDEV(second_major, 0), 1);
 }
 
 module_exit(second_exit);
+
+
+MODULE_AUTHOR("ldd");
+MODULE_LICENSE("GPL v2");
+
